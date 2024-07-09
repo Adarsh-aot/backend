9e79d83aa5c6590d6c70c6ef92fa7f93ff559911
Adarsh-aot <160816474+Adarsh-aot@users.noreply.github.com>
2024-07-09 04:14:05 -0700

    Update order_route.js

--- routes/order_route.js
+++ routes/order_route.js
@@ -5,6 +5,8 @@ const order = require('../helper/order')
 const { route } = require('./registration_route')
 const { jwt_token_validatotion } = require('../middleware/jwt_token')
 
+
+
 router.get('/myorder',jwt_token_validatotion,order.myorder)
 router.post('',jwt_token_validatotion,order.addorder)
 router.get('',jwt_token_validatotion,order.getorder)
