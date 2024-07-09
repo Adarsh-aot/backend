8a05beb71ac9ed174058a3f17896cfdb5b1a0867
Adarsh-aot <160816474+Adarsh-aot@users.noreply.github.com>
2024-07-08 23:07:51 -0700

    Update Admin.js

--- helper/Admin.js
+++ helper/Admin.js
@@ -4,7 +4,7 @@ const { todo } = require("../constant/array");
 const getdata = (req , res) =>    {
     const id = req.decode.id
     const type = todo[id].type
-    let admindet = todo.filter((item) => item.type != type)
+    let admindet = todo.filter((item) => item.type == type)
     res.json(admindet)
 }
 
